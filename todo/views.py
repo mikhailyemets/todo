8f9bae516a5f84f78304f79eeee4cd109870aa45
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-05-22 10:42:34 +0200

    changed index.html so it looks up to requirements

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from .models import Task, Tag
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    """View function for the home page of the site."""
-
-    return render(request, "todo/index.html")
+def index(request):
+    tasks = Task.objects.all().prefetch_related('tags')
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'todo/index.html', context)
