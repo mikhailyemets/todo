b90dcca52f4a9273c965e224d6787f413c4190d3
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-05-22 11:54:36 +0200

    add button add/delete/update for task

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,73 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import TaskForm, TagForm, TaskUpdateForm
 from .models import Task, Tag
 
 
-def index(request):
+def index(request:HttpRequest) -> HttpResponse:
     tasks = Task.objects.all().prefetch_related('tags')
     context = {
         'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
+
+
+def add_task(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo:index')
+    else:
+        form = TaskForm()
+
+    return render(request, 'todo/add_task.html', {'form': form})
+
+
+def add_tag(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo:tag_list')
+    else:
+        form = TagForm()
+
+    return render(request, 'todo/add_tag.html', {'form': form})
+
+
+def tag_list(request: HttpRequest) -> HttpResponse:
+    tags = Tag.objects.all()
+    return render(request, 'todo/tag_list.html', {'tags': tags})
+
+
+def update_tag(request: HttpRequest, pk: int) -> HttpResponse:
+    tag = Tag.objects.get(id=pk)
+    return redirect('todo:tag_list')
+
+
+def delete_tag(request: HttpRequest, pk: int) -> HttpResponse:
+    tag = Tag.objects.get(id=pk)
+    tag.delete()
+    return redirect('todo:tag_list')
+
+
+def update_task(request:HttpRequest, pk: int) -> HttpResponse:
+    task = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        form = TaskUpdateForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('todo:index')
+    else:
+        form = TaskUpdateForm(instance=task)
+
+    return render(request, 'todo/update_task.html', {'form': form})
+
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return redirect('todo:index')
--- todo_pr/settings.py
+++ todo_pr/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Crispy forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
