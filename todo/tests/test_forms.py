cb8c391c1413d24956ede6bea371fbb23b9d7c79
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-05-22 14:21:01 +0200

    add unit-tests

--- todo/tests/test_forms.py
+++ todo/tests/test_forms.py
@@ -1,16 +1,28 @@
 from django.test import TestCase
 from ..models import Task, Tag
 from ..forms import TaskForm, TagForm, TaskUpdateForm
+from django.contrib.auth import get_user_model
 
 
 class TaskFormTestCase(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser", password="testpassword"
+        )
+
     def test_task_form_valid(self):
-        form_data = {'content': 'Test Task', 'deadline': '2022-12-31 23:59:59', 'is_done': False, 'tags': []}
+        form_data = {
+            "content": "Test Task",
+            "deadline": "2022-12-31 23:59:59",
+            "is_done": False,
+            "tags": [],
+            "owner": self.user.id,
+        }
         form = TaskForm(data=form_data)
         self.assertTrue(form.is_valid())
 
     def test_tag_form_valid(self):
-        form_data = {'name': 'Test Tag'}
+        form_data = {"name": "Test Tag"}
         form = TagForm(data=form_data)
         self.assertTrue(form.is_valid())
 
@@ -23,12 +35,14 @@ class TaskFormTestCase(TestCase):
         form_data = {}
         form = TagForm(data=form_data)
         self.assertFalse(form.is_valid())
-class TaskUpdateFormTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.task = Task.objects.create(content='Test Task', is_done=False)
 
     def test_task_update_form_valid(self):
-        form_data = {'content': 'Test Task Updated', 'is_done': True, 'deadline': '2022-12-31 23:59:59'}
-        form = TaskUpdateForm(data=form_data, instance=self.task)
+        task = Task.objects.create(content="Test Task", is_done=False, owner=self.user)
+        form_data = {
+            "content": "Test Task Updated",
+            "is_done": True,
+            "deadline": "2022-12-31 23:59:59",
+            "owner": self.user.id,
+        }
+        form = TaskUpdateForm(data=form_data, instance=task)
         self.assertTrue(form.is_valid())
--- todo/views.py
+++ todo/views.py
@@ -30,81 +30,80 @@ def index(request: HttpRequest) -> HttpResponse:
         "is_paginated": paginator.num_pages > 1,
     }
 
-    return render(request, 'todo/index.html', context)
-
+    return render(request, "todo/index.html", context)
 
 
 def add_task(request: HttpRequest) -> HttpResponse:
-    if request.method == 'POST':
+    if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('todo:index')
+            return redirect("todo:index")
     else:
         form = TaskForm()
 
-    return render(request, 'todo/add_task.html', {'form': form})
+    return render(request, "todo/add_task.html", {"form": form})
 
 
 def add_tag(request: HttpRequest) -> HttpResponse:
-    if request.method == 'POST':
+    if request.method == "POST":
         form = TagForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('todo:tag_list')
+            return redirect("todo:tag_list")
     else:
         form = TagForm()
 
-    return render(request, 'todo/add_tag.html', {'form': form})
+    return render(request, "todo/add_tag.html", {"form": form})
 
 
 def tag_list(request: HttpRequest) -> HttpResponse:
     tags = Tag.objects.all()
-    return render(request, 'todo/tag_list.html', {'tags': tags})
+    return render(request, "todo/tag_list.html", {"tags": tags})
 
 
 def update_tag(request: HttpRequest, pk: int) -> HttpResponse:
     tag = Tag.objects.get(id=pk)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = TagForm(request.POST, instance=tag)
         if form.is_valid():
             form.save()
-            return redirect('todo:tag_list')
+            return redirect("todo:tag_list")
     else:
         form = TagForm(instance=tag)
 
-    return render(request, 'todo/update_tag.html', {'form': form, 'tag': tag})
+    return render(request, "todo/update_tag.html", {"form": form, "tag": tag})
 
 
 def delete_tag(request: HttpRequest, pk: int) -> HttpResponse:
     tag = Tag.objects.get(id=pk)
     tag.delete()
-    return redirect('todo:tag_list')
+    return redirect("todo:tag_list")
 
 
-def update_task(request:HttpRequest, pk: int) -> HttpResponse:
+def update_task(request: HttpRequest, pk: int) -> HttpResponse:
     task = Task.objects.get(id=pk)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = TaskUpdateForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
-            return redirect('todo:index')
+            return redirect("todo:index")
     else:
         form = TaskUpdateForm(instance=task)
 
-    return render(request, 'todo/update_task.html', {'form': form})
+    return render(request, "todo/update_task.html", {"form": form})
 
 
 def delete_task(request, pk):
     task = Task.objects.get(id=pk)
     task.delete()
-    return redirect('todo:index')
+    return redirect("todo:index")
 
 
-def toggle_task_status(request:HttpRequest, pk: int) -> HttpResponse:
+def toggle_task_status(request: HttpRequest, pk: int) -> HttpResponse:
     task = Task.objects.get(id=pk)
     task.is_done = not task.is_done
     task.save()
-    return redirect('todo:index')
+    return redirect("todo:index")
